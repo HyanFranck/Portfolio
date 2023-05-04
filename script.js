2ca1b69a922827bfd4f2aaec68849df1aaa341f7
Hyan <hfranck.jr3@gmail.com>
2023-05-04 15:03:44 -0300

    Portfólio

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ links.forEach((link) => {
         checkbox.checked = false;
     });
 });
+
+// Obtenha todas as divs com a classe "card-wrapper"
+const cardWrappers = document.querySelectorAll('.card-wrapper');
+
+// Adicione um evento onclick a cada div
+cardWrappers.forEach((cardWrapper) => {
+  cardWrapper.onclick = () => {
+    // Obtenha o nome do arquivo da página que você deseja redirecionar
+    const fileName = 'Projetos1/Index.html';
+
+    // Redirecione para a página desejada dentro da mesma pasta
+    window.location.href = `${window.location.origin}/${fileName}`;
+  }
+});
+
+

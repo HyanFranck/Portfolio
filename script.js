c6bb169e433cbaac4df0e33b28acaae446cad4aa
hyan Franck <hfranck.jr3@gmail.com>
2023-05-03 22:35:40 -0300

    Portfólio

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const links = document.querySelectorAll('.menu-itens a');
 
 links.forEach((link) => {
-  link.addEventListener('click', () => {
-    const checkbox = document.getElementById('toggle');
-    checkbox.checked = false;
-  });
+    link.addEventListener('click', () => {
+        const checkbox = document.getElementById('toggle');
+        checkbox.checked = false;
+    });
 });
